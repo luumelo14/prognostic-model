6c505e4b70fd6a88876884cc5c8c960a3069f00a
Luciana <lucidemelo@hotmail.com>
2018-07-24 17:28:14 -0300

    Remove unused functions

--- comparison.py
+++ comparison.py
@@ -9,12 +9,8 @@ import plot
 from sklearn import  tree
 import randomForest as rf
 import decisionTree as dt
-#import MTdecisionTree as mtdt
-#import MTrandomForest as mtrf
 from scipy.stats import sem
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import AdaBoostClassifier
-#from sklearn.metrics import accuracy_score, classification_report
 from sklearn.svm import SVC 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
@@ -28,28 +24,41 @@ import pickle
 # DOI=http://dx.doi.org/10.1016/j.patrec.2010.03.014  
 def feature_selection_threshold(X,y,attributes,ntrees,replace,mtry,max_depth,missing_branch,balance,
     cutoff,ntimes=25,title=None,missing_rate=False,vitype='err',vimissing=True,backwards=False,save_models=False):
+    
+    # get average feature importances for each feature
     vis =  average_varimp(X,y,attributes,ntrees,replace,mtry,max_depth,missing_branch,
         missing_rate=missing_rate,ntimes=ntimes,select=False,mean=False,vitype=vitype,vimissing=vimissing,printvi=False)
 
-    if(backwards):
+    # if backwards is True, then the feature selection will start the process with all features,
+    # and eliminating the least important ones in each step
+    if(backwards is True):
         reverse=False
         comp_threshold = lambda x,y: x <= y
         get_slice = lambda x,index: x[index:] 
         stop_index=-1
+        chosen_model = -1
+    # if it's False, then it starts with only the most important feature, and then adding
+    # the most important ones in each step
     else:
-        reverse=True
+        reverse = True
         comp_threshold = lambda x,y: x > y
         get_slice = lambda x: x[0:index]
-        stop_index=0
+        stop_index = 0
+        chosen_model = 0
+
     ordered_features = [a[0] for a in sorted(vis,key=lambda x:np.mean(x[1]),reverse=reverse)]
-    thresholds =  [round(np.mean(vis[a][1]),10) for a in ordered_features]
+    thresholds =  [np.mean(vis[a][1]) for a in ordered_features] 
     threshold_values = sorted([round(a,10) for a in set(thresholds)],reverse=reverse)
-    #print(sorted([(attributes[a[0]],round(np.mean(a[1]),10)) for a in vis],key=lambda x: x[1],reverse=True))
- 
+
     stop_indexes = []
     scores = []
     i = 0
     nn = 0
+    # for each threshold value (feature importance value), create a forest
+    # only using: (a) features whose importance value is <= than the threshold
+    # if backwards is True (starting from the least important), or
+    # (b) features whose importance value is > than the threshold if backwards is False
+    # (starting from the most important one)
     for threshold in threshold_values:
 
         s_index = stop_index+1
@@ -66,34 +75,21 @@ def feature_selection_threshold(X,y,attributes,ntrees,replace,mtry,max_depth,mis
             missing_branch=missing_branch,prob_answer=False,max_depth=max_depth,replace=replace,balance=balance,
             cutoff=cutoff)
 
-        if(i == X.shape[0]):
-            i = 0
    
-        clf.fit(np.append(X[0:i,features],X[i+1:,features],axis=0),np.append(y[0:i],y[i+1:]))
+        clf.fit(X[:,features],y)
         scores.append(1-clf.oob_error_)
         if(save_models is True):
             with open('prognostic_model_' + title+ str(nn) + '.pickle', 'wb') as handle:
                 pickle.dump(clf,handle)
-        nn+=1
+            nn+=1
         stop_indexes.append(stop_index)
 
-    #print('Best oob errors:')
-    #for old_index in np.where(np.array(scores) == max(scores))[0]:
-        #print('Features:')
-        #print((attributes[ordered_features[0:len(ordered_features)-old_index]]))
-    #    print(scores[old_index])
 
-    #print('Best 1 s.e. set of features:')
     stdm = sem(scores)
     indexes= np.where(np.array(scores) == scores[((np.abs(np.array([a for a in scores if a != max(scores)])-(max(scores)-stdm))).argmin())])[0]
-    index = indexes[-1]
-
-    #print(scores[index])
-    
-    #index = indexes[int(len(indexes)/2)]
-    #index = max(indexes,key=lambda x: stop_indexes[x])
-    #print('Features:')
-    #print(attributes[ordered_features[0:len(ordered_features)-index]])
+    # the forest with the best score (closest to the max score subtracted from the standard error of scores) and 
+    # the biggest threshold value (by index -1) is chosen as the suggested model to be used 
+    index = indexes[chosen_model]
 
     seed = np.random.randint(0,10000)
     clf = rf.RandomForest(ntrees=ntrees,oob_error=True,random_state=seed,mtry=mtry,
@@ -103,28 +99,13 @@ def feature_selection_threshold(X,y,attributes,ntrees,replace,mtry,max_depth,mis
     clf.attributes = attributes[get_slice(ordered_features,stop_indexes[index])]
     clf.fit(X[:,get_slice(ordered_features,stop_indexes[index])],y)
 
-    #print('OOB ERROR: %r' % (1-clf.oob_error_))
-    #print('threshold: >= %r' % threshold_values[index])
-    importance_values = [[round(np.mean(aa),10) for aa in a[1]] for a in vis if round(np.mean(a[1]),10) >= threshold_values[index]]
-    features =  attributes[[a[0] for a in vis if round(np.mean(a[1]),10) >= threshold_values[index]]]
+    #importance_values = [[round(np.mean(aa),10) for aa in a[1]] for a in vis if round(np.mean(a[1]),10) >= threshold_values[index]]
+    #features =  attributes[[a[0] for a in vis if round(np.mean(a[1]),10) >= threshold_values[index]]]
 
     #plot.boxplot(importance_values,features,title)
     #plot.plot_feature_importance_vs_accuracy(threshold_values,scores,xlabel='threshold',title=title,special=index)
     
     return clf
-
-
-def plot_boxplot(X,y,attributes,ntrees,replace,mtry,max_depth,missing_branch,ntimes=25,ptype='fc',title=None):
-    if(ptype == 'fc'):
-        vis = average_fc(X,y,ntrees,replace,mtry,max_depth,missing_branch,ntimes)
-    else:
-        vis = average_varimp(X,y,ntrees,replace,mtry,max_depth,missing_branch,ntimes,select=False)
-
-    importance_values = [a[1] for a in vis]
-    features = attributes[[a[0] for a in vis]]
-
-    import plot
-    plot.boxplot(importance_values,features,title)
 
 
 
@@ -180,32 +161,6 @@ def average_varimp(X,y,attributes,ntrees,replace,mtry,max_depth,missing_branch,v
 
     return sorted(vi.items(),key=lambda x: x[0])
 
-def average_fc(X,y,ntrees,replace,mtry,max_depth,missing_branch,ntimes=25):
-    feature_contributions = {a: [] for a in range(X.shape[1])}
-    if ('N' in set(y)):
-        class_of_interest = 'N'
-    elif ('Sucesso' in set(y)):
-        class_of_interest = 'Sucesso'
-    else:
-        class_of_interest = set(y)[0]
-        print('class of interest set to be %r' % class_of_interest)
-
-    for i in range(ntimes):
-        seed = np.random.randint(0,10000)
-        clf = rf.RandomForest(ntrees=ntrees,oob_error=True,random_state=seed,mtry=mtry,
-            missing_branch=missing_branch,prob_answer=False,max_depth=max_depth,replace=replace,balance=True)
-        clf.fit(X,y)
-        fcs = clf.feature_contribution()
-        for i in range(len(fcs)):
-            for f in fcs[i].keys():
-                feature_contributions[f].append(fcs[i][f][class_of_interest])
-
-    # for f in feature_contributions.keys():
-    #     if(len(feature_contributions[f]) == 0):
-    #         print(f)
-    #     feature_contributions[f] = np.mean(feature_contributions[f])
-
-    return sorted(feature_contributions.items(),key=lambda x: x[0],reverse=True)
 
 def compare_models(X,y,class_name,transform=False,scale=False,n_splits=10,test_size=None,random_state=9,use_feature_selection=False):
    
@@ -303,39 +258,6 @@ def select_params(X,y):
     for a,b in zip(np.array(parameters)[np.array(final_scores).argsort()[:10]], np.array(sorted(final_scores)[:10])):
         print('%r : %r ' % (a,b))
 
-def check_feature_rate(X,y,attributes,ntrees,replace,mtry,max_depth,missing_branch):
-
-    seed = np.random.randint(0,10000)
-    clf1 = rf.RandomForest(ntrees=ntrees,oob_error=True,random_state=seed,
-        mtry=mtry,missing_branch=missing_branch,prob_answer=False,max_depth=max_depth,replace=replace,balance=True)
-    clf1.fit(X,y)
-    attributes_used = {}
-    for tree in clf1.forest:
-        
-        for attribute in tree.feature_indices:
-            if(attribute not in attributes_used.keys()):
-                attributes_used[attribute] = 1
-            else:
-                attributes_used[attribute] += 1
-
-    if(len((attributes_used.keys())) != X.shape[1]):
-        print(len(attributes_used.keys()))
-        print(X.shape[1])
-        print('not equal!!! %r' % (1-len(attributes_used.keys())/X.shape[1]))
-    print({attributes[a]: b for a,b in attributes_used.items()})
-    print(1-clf1.oob_error_)
-
-def check_other_participants(filename):
-    import pandas as pd
-    p = []
-    new_data = pd.read_csv('Dados/All_New_Participants.csv')
-    data = pd.read_csv('EXPORT/NES_EXPORT_EntradaUnificada_11_04_2017/Questionnaire_data/Q44071_Avaliação-de-Entrada-Unificada/Responses_Q44071.csv')
-
-    for participant in new_data[new_data.columns[0]]:
-        if(not np.any(participant == data['participant_code'])):
-            p.append(participant)
-            print(participant)
-    print(len(p))
 
 data_paths = [['../DorCirurgiaCategNAReduzido.csv','Q92510_snDorPos'],
 ['../AbdOmbroCirurgiaCategNAReduzido.csv','Q92510_opcForca[AbdOmbro]'],
@@ -377,6 +299,11 @@ for data_path,class_name in data_paths:
     vitype = 'auc'
 
     print('--------------- MODEL: %r DATA PATH: %r' % (class_name, data_path))
+    clf1 = rf.RandomForest(ntrees=ntrees,oob_error=True,random_state=seed,
+        mtry=mtry,missing_branch=missing_branch,prob_answer=False,max_depth=max_depth,replace=replace,balance=True)
+    clf1.fit(X,y)
+    print(clf1.oob_error_)
+    exit()
     # if('Ombro' in data_path):
     #     vitype = 'auc'
     # else:
